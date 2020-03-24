1d740857cbaab0f9273eab72b3d77f6f8a9a33a1
Ramiro Gustavo Lopez <ramlopez@everis.com>
2020-03-24 19:42:33 -0300

    Added function CRUD to post component

--- api/components/post/controller.js
+++ api/components/post/controller.js
@@ -1,6 +1,6 @@
 const nanoid = require('nanoid')
 const auth = require('../auth')
-const TABLA = 'post'
+const TABLE = 'post'
 
 module.exports = function (injectedStore) {
     let store = injectedStore;
@@ -9,10 +9,36 @@ module.exports = function (injectedStore) {
     }
 
     function list(){
-        return store.list(TABLA)
+        return store.list(TABLE)
     }
+
+    function get(id){
+        return store.get(TABLE, id)
+    }
+
+    function upsert(post){
+     console.log('Data on upsert Controller post: ', post)
+        if(post.id){
+            ///update
+            return store.upsert(TABLE,post)
+        }else{
+           return store.upsert(TABLE,post)
+        }
+
+    }
+
+
+/*****
+ * TODO: Create the rest of the functions:
+ *  upsert,
+ *  remove
+ */
+
     
     return{
         list,
+        get,
+        upsert,
+        //remove,
     }
 }
--- api/components/post/network.js
+++ api/components/post/network.js
@@ -6,13 +6,44 @@ const controller = require('./index')
 const router = express.Router();
 
 //Routes 
-router.get('/', list);
+router.get('/', list)
+router.get('/:id', getPost)
+router.put('/:id', upsert)
+router.post('/', upsert)
+router.delete('/:id',del)
 
 //Functions
 function list(req, res, next) {
     controller.list()
         .then((list) => {
             response.success(req, res, 200, list)
+        })
+        .catch(next)
+}
+
+function getPost(req, res, next) {
+    controller.get(req.params.id)
+        .then((post) => {
+            response.success(req, res, 200, post)
+        })
+        .catch(next)
+}
+
+function upsert(req,res,next){
+    let param= req.params.id
+    let data= req.body
+    //console.log('This is the data to be inserted: ',data)
+    controller.upsert(data)
+        .then((result)=>{
+            response.success(req,res,200,result)
+        })
+        .catch(next)
+}
+
+function del(req,res,next){
+    controller.remove(id)
+        .then((result)=>{
+            response.success(req,res,200,result)
         })
         .catch(next)
 }
